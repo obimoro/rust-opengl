c0fcff6e1c643ab631006e1011c10360da061c1c
Andreas Langseth <andreas.langseth@nrk.no>
2025-06-12 14:47:20 +0200

    feature: my first rust triangle

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,23 @@ use glfw::{Action, Context, Key, WindowEvent};
 use gl::types::*;
 
 
+
 const WIN_WIDTH: u32 = 640;
 const WIN_HEIGHT: u32 = 480;
+
+const VERTEX_SHADER_SOURCE: &str = "#version 330 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}";
+
+const FRAGMENT_SHADER_SOURCE: &str = "#version 330 core
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}";
 
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
@@ -22,31 +37,112 @@ fn main() {
     // set the viewport for openGL
     unsafe {
         gl::Viewport(0, 0,WIN_WIDTH as i32, WIN_HEIGHT as i32);
-        gl::ClearColor(1.0, 0.5, 0.0, 1.0);
     }
-
+    
+    // build and compile shader program
+    // vertex shader
+    let vertex_shader = unsafe {
+        let shader: u32 = gl::CreateShader(gl::VERTEX_SHADER);
+        let c_str_vert = std::ffi::CString::new(VERTEX_SHADER_SOURCE).unwrap();
+        gl::ShaderSource(shader, 1, &c_str_vert.as_ptr(), std::ptr::null());
+        gl::CompileShader(shader);
+        shader
+    };
+    
+    // fragment shader
+    let fragment_shader = unsafe {
+        let shader: u32 = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let c_str_vert = std::ffi::CString::new(FRAGMENT_SHADER_SOURCE).unwrap();
+        gl::ShaderSource(shader, 1, &c_str_vert.as_ptr(), std::ptr::null());
+        gl::CompileShader(shader);
+        shader
+    };
+    
+    let shader_Program = unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vertex_shader);
+        gl::AttachShader(program, fragment_shader);
+        gl::LinkProgram(program);
+        // check for linking erros
+        let mut success = 0;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut success);
+        if success == 0 {
+            println!("ERROR::SHADER::PROGRAM::LINKING_FAILED")
+        }
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+        program
+    };
+    
+    // vertex data
+    let vertices: [f32; 9] = [ 
+        -0.5,-0.5, 0.0,
+         0.5,-0.5, 0.0,
+         0.0, 0.5, 0.0];
+    
+    // VBO
+    let mut vbo: u32 = 0;
+    unsafe {
+        // 0. copy vertices array in a buffer for openGL to use
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        // 1. set the vertex attributes pointers
+        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * std::mem::size_of::<f32>()) as isize,
+        vertices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 
+                            3 * std::mem::size_of::<f32>() as i32, std::ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // 2. use shader program when we want to render an object
+        gl::UseProgram(shader_Program);
+    }
+    
+    // VAO
+    let mut vao: u32 = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        // 1. bind vertex array object
+        gl::BindVertexArray(vao);
+        // 2. copy vertices array in a buffer for openGL to use
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * std::mem::size_of::<f32>()) as isize, vertices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
+        // 3. set our vertex attributes pointers
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * std::mem::size_of::<f32>() as i32, std::ptr::null());
+        gl::EnableVertexAttribArray(0);
+    }
+    
     // Loop until the user closes the window
     while !window.should_close() {
         
         // input
-        let (mouse_x, mouse_y) =  window.get_cursor_pos();
-        println!("Mouse pos X {}, Mouse pos Y {}", mouse_x, mouse_y);
-        // Rendering commands here
-        
-        // Clear the window
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        
-        }
-
-        // Poll for and process events
-        glfw.poll_events();
         for (_, event)  in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
         }
+        
+        // Rendering commands here
+        unsafe {
+            // default clear color
+            gl::ClearColor(0.5, 1.0, 0.0, 1.0);
+            // Clear the window
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            // link shaders
+            gl::UseProgram(shader_Program);
+            // bind vertex array object
+            gl::BindVertexArray(vao);
+            // Draw triangle
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            
+        }
+        
+        
+        
+        // Poll for and process events
+        glfw.poll_events();
         // swap front and back buffers
         window.swap_buffers();
     }
+
+    // Cleanup and housekeeping
+    
 }
 
 
@@ -54,6 +150,16 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
     match event {
         glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
             window.set_should_close(true);
+        }
+        glfw::WindowEvent::Key(Key::Num1, _, Action::Press, _) => {
+            unsafe {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+            }
+        }
+        glfw::WindowEvent::Key(Key::Num2, _, Action::Press, _) => {
+            unsafe {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL);
+            }
         }
         _ => {}
     }
