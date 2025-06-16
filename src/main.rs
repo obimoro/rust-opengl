f95f7a0279ae01a646c5b4b0ca56dbb2590ff157
Andreas Langseth <andreas.langseth@nrk.no>
2025-06-16 08:56:44 +0200

    feautre: a triangle, now in color

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,38 @@
 //
 // -------------------------------------------------------------
 use glfw::{Action, Context, Key, WindowEvent};
+use glfw::ffi::glfwGetTime;
+extern crate gl;
 use gl::types::*;
 
 
 
+// global values
 const WIN_WIDTH: u32 = 640;
 const WIN_HEIGHT: u32 = 480;
 
 const VERTEX_SHADER_SOURCE: &str = "#version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec3 ourColor;
+
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos, 1.0);
+    ourColor = aColor;
 }";
 
 const FRAGMENT_SHADER_SOURCE: &str = "#version 330 core
 out vec4 FragColor;
+in vec3 ourColor;
+
 void main()
 {
-    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+    FragColor = vec4(ourColor, 1.0);
 }";
 
+// Main loop
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
@@ -39,11 +50,18 @@ fn main() {
     // init GL
     let gl = gl::load_with(|s| window.get_proc_address(s) as *const _);
     
+
+    let mut nrAttributes = 0;
+    unsafe {
+        gl::GetIntegerv(gl::MAX_VERTEX_ATTRIBS, &mut nrAttributes);
+    }
+    println!("Maximum nr of vertex attributes supported: {}", nrAttributes);
+
     // set the viewport for openGL
     unsafe {
         gl::Viewport(0, 0,WIN_WIDTH as i32, WIN_HEIGHT as i32);
     }
-    
+
     // build and compile shader program
     // vertex shader
     let vertex_shader = unsafe {
@@ -80,60 +98,40 @@ fn main() {
     };
     
     // vertex data
-    let vertices: [f32; 12] = [ 
-         0.5, 0.5, 0.0,
-         0.5,-0.5, 0.0,
-        -0.5,-0.5, 0.0,
-        -0.5, 0.5, 0.0];
-    let indices: [i32; 6] = [
-        0, 1, 3,
-        1, 2, 3
-    ];
+    let vertices: [f32; 18] = [ 
+         0.5,-0.5, 0.0, 1.0, 0.0, 0.0,
+        -0.5,-0.5, 0.0, 0.0, 1.0, 0.0,
+         0.0, 0.5, 0.0, 0.0, 0.0, 1.0];
     
     // VBO
     let mut vbo: u32 = 0;
-    unsafe {
-        // 0. copy vertices array in a buffer for openGL to use
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        // 1. set the vertex attributes pointers
-        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * std::mem::size_of::<f32>()) as isize,
-        vertices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 
-                            3 * std::mem::size_of::<f32>() as i32, std::ptr::null());
-        gl::EnableVertexAttribArray(0);
-        // 2. use shader program when we want to render an object
-        gl::UseProgram(shader_Program);
-    }
     
-    // EBO
-    let mut ebo: u32 = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut ebo);
-    }
-
     // VAO
     let mut vao: u32 = 0;
     unsafe {
         gl::GenVertexArrays(1, &mut vao);
+        // 0. copy vertices array in a buffer for openGL to use
+        gl::GenBuffers(1, &mut vbo);
         // 1. bind vertex array object
         gl::BindVertexArray(vao);
-        // 2. copy vertices array in a buffer for openGL to use
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * std::mem::size_of::<f32>()) as isize, vertices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
+        // 1. set the vertex attributes pointers
+        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * std::mem::size_of::<f32>()) as isize,
+                                                        vertices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
         // 3. set our vertex attributes pointers
-
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-        gl::BufferData(gl::ELEMENT_ARRAY_BUFFER, (indices.len() * std::mem::size_of::<i32>()) as isize, indices.as_ptr() as *const std::ffi::c_void, gl::STATIC_DRAW);
-
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * std::mem::size_of::<f32>() as i32, std::ptr::null());
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 6 * std::mem::size_of::<f32>() as i32, std::ptr::null());
         gl::EnableVertexAttribArray(0);
+        // color attribute
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 6 * std::mem::size_of::<f32>() as i32, (3 * std::mem::size_of::<f32>() as i32) as *const std::ffi::c_void);
+        gl::EnableVertexAttribArray(1);
+        // 2. use shader program when we want to render an object
+        gl::UseProgram(shader_Program);
     }
 
     
     // Loop until the user closes the window
     while !window.should_close() {
-        
         // input
         for (_, event)  in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
@@ -154,15 +152,13 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
             // link shaders
             gl::UseProgram(shader_Program);
+
             // bind vertex array object
             gl::BindVertexArray(vao);
             // Draw triangle
-            // gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            // Draw rect
-            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
+             gl::DrawArrays(gl::TRIANGLES, 0, 3);
             
         }
-        
         
         
         // Poll for and process events
@@ -172,6 +168,13 @@ fn main() {
     }
 
     // Cleanup and housekeeping
+    unsafe {
+        gl::DeleteVertexArrays(1, &vao);
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteProgram(shader_Program);
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+    }
     
 }
 
